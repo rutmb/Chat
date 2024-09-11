2a744dfd43f1b4657b59c6ae917e67620c3fd8ca
Igor Rudenko <tamboff69@gmail.com>
2024-09-11 10:24:49 +0300

    Fixed the scroll to bottom crash

--- Sources/ExyteChat/ChatView/UIList.swift
+++ Sources/ExyteChat/ChatView/UIList.swift
@@ -66,7 +66,7 @@ struct UIList<MessageContent: View, InputView: View>: UIViewRepresentable {
 
         NotificationCenter.default.addObserver(forName: .onScrollToBottom, object: nil, queue: nil) { _ in
             DispatchQueue.main.async {
-                if !context.coordinator.sections.isEmpty {
+                if let firstSection = context.coordinator.sections.first, !firstSection.rows.isEmpty {
                     tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .bottom, animated: true)
                 }
             }
