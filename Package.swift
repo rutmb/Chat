c4edd337236e75012fbd63764091a47df079e5e8
Igor Rudenko <tamboff69@gmail.com>
2025-09-23 08:51:51 +0300

    Updated SwiftUI-introspect dependency

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/siteline/swiftui-introspect",
-            from: "1.1.3"
+            from: "26.0.0"
         ),
         .package(
             url: "https://github.com/exyte/MediaPicker.git",
